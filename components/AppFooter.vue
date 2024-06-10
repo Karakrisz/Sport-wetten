f37a097107e99acbb6b9d00296f2348ba48ce0c7
Boaz <webevolution13@gmail.com>
2024-06-10 18:49:29 +0200

    upDate
    front end

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,46 @@
 <script setup></script>
 
 <template>
-  <footer>lábléc</footer>
+  <footer>
+    <div class="footer-content grid-4">
+      <div class="footer-content__ilBox">
+        <NuxtImg
+          src="/img/footer/logo.svg"
+          alt="Sport wetten"
+          class="footer-content__ilBox__img"
+          height="100%"
+        />
+
+        <div class="footer-content__ilBox__social-links text-center">
+          <a href="#">
+            <NuxtImg
+              src="/img/footer/instagram.svg"
+              alt="Sport wetten"
+              class="footer-content__ilBox__social-links__img"
+              height="100%"
+            />
+          </a>
+          <a href="#">
+            <NuxtImg
+              src="/img/footer/facebook.svg"
+              alt="Sport wetten"
+              class="footer-content__ilBox__social-links__img"
+              height="100%"
+            />
+          </a>
+          <a href="#">
+            <NuxtImg
+              src="/img/footer/youtube.svg"
+              alt="Sport wetten"
+              class="footer-content__ilBox__social-links__img"
+              height="100%"
+            />
+          </a>
+        </div>
+      </div>
+      <div>valami</div>
+      <div>valami</div>
+      <div>valami</div>
+    </div>
+  </footer>
 </template>
