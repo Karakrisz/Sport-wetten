c9ad5891756f3b056c4ac12bdb8c27211e6279c6
Boaz <webevolution13@gmail.com>
2024-06-10 19:35:01 +0200

    upDate
    front end

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,7 +2,7 @@
 
 <template>
   <footer>
-    <div class="footer-content grid-4">
+    <div class="footer-content d-flex pr">
       <div class="footer-content__ilBox">
         <NuxtImg
           src="/img/footer/logo.svg"
@@ -38,9 +38,73 @@
           </a>
         </div>
       </div>
-      <div>valami</div>
-      <div>valami</div>
-      <div>valami</div>
+      <div class="footer-content__linkBox">
+        <h5
+          class="footer-content__linkBox__h5 f-600 text-transform-uppercase text-color-w"
+        >
+          INFORMATIONAL
+        </h5>
+        <p class="footer-content__linkBox__p">
+          <NuxtLink class="footer-content__linkBox__p__link text-color-w">
+            About Us
+          </NuxtLink>
+        </p>
+        <p class="footer-content__linkBox__p">
+          <NuxtLink class="footer-content__linkBox__p__link text-color-w">
+            Contact
+          </NuxtLink>
+        </p>
+        <p class="footer-content__linkBox__p">
+          <NuxtLink class="footer-content__linkBox__p__link text-color-w">
+            Privacy Policy
+          </NuxtLink>
+        </p>
+        <p class="footer-content__linkBox__p">
+          <NuxtLink class="footer-content__linkBox__p__link text-color-w">
+            FAQ
+          </NuxtLink>
+        </p>
+      </div>
+
+      <div class="footer-content__linkBox">
+        <h5
+          class="footer-content__linkBox__h5 f-600 text-transform-uppercase text-color-w"
+        >
+          LEGAL
+        </h5>
+        <p class="footer-content__linkBox__p">
+          <NuxtLink class="footer-content__linkBox__p__link text-color-w">
+            Cookie Policy
+          </NuxtLink>
+        </p>
+        <p class="footer-content__linkBox__p">
+          <NuxtLink class="footer-content__linkBox__p__link text-color-w">
+            Data Protection
+          </NuxtLink>
+        </p>
+        <p class="footer-content__linkBox__p">
+          <NuxtLink class="footer-content__linkBox__p__link text-color-w">
+            Terms and Conditions
+          </NuxtLink>
+        </p>
+      </div>
+
+      <div class="footer-content__linkBox">
+        <h5
+          class="footer-content__linkBox__h5 f-600 text-transform-uppercase text-color-w"
+        >
+          NAVIGATION
+        </h5>
+      </div>
+
+      <div class="footer-content__DigitalSeedBrand pa">
+        <NuxtImg
+          src="/img/footer/digitalSeed.svg"
+          alt="Sport wetten"
+          class="footer-content__DigitalSeedBrand__img"
+          height="100%"
+        />
+      </div>
     </div>
   </footer>
 </template>
